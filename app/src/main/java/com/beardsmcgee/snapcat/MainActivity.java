cd2266745a1046634431decb3a06e71407e45f31
Eric Wickstrom <wickstrome@gmail.com>
2016-08-12 19:59:55 -0500

    begin implementing authentication

--- app/src/main/java/com/beardsmcgee/snapcat/MainActivity.java
+++ app/src/main/java/com/beardsmcgee/snapcat/MainActivity.java
@@ -1,13 +1,35 @@
 package com.beardsmcgee.snapcat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Firebase
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize firebase
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        //If current user is null, start login activity
+        if(mFirebaseUser == null){
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        } else {
+            // update activity with user info
+            // ie: username, photo id, etc
+        }
     }
 }

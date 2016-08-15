bfac833ad397b8213e186e4ec0cbb4ca1ef9f679
Eric Wickstrom <wickstrome@gmail.com>
2016-08-14 22:32:07 -0500

    began database implementation.  basic read/write so far.

---
+++ app/src/main/java/com/beardsmcgee/snapcat/Constants.java
@@ -0,0 +1,8 @@
+package com.beardsmcgee.snapcat;
+
+/**
+ * Created by beardsmcgee on 8/14/16.
+ */
+public class Constants {
+    public static final String FIREBASE_CHILD_POSTS = "posts";
+}
--- app/src/main/java/com/beardsmcgee/snapcat/MainActivity.java
+++ app/src/main/java/com/beardsmcgee/snapcat/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,9 +20,14 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity
-        implements GoogleApiClient.OnConnectionFailedListener{
+        implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
     //GoogleApiClient
     GoogleApiClient mGoogleApiClient;
@@ -26,10 +35,16 @@ public class MainActivity extends AppCompatActivity
     // Firebase
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabaseReference;
 
     //Views
-    TextView nameTextView;
-    ImageView photoImageView;
+    private TextView nameTextView;
+    private ImageView photoImageView;
+    private EditText editText;
+    private Button mSignInButton;
+
+    //temp  todo: delete this
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,10 @@ public class MainActivity extends AppCompatActivity
         //Views
         nameTextView = (TextView) findViewById(R.id.mainActivityNameTV);
         photoImageView = (ImageView) findViewById(R.id.mainActivityPhotoIv);
+        editText = (EditText) findViewById(R.id.mainActivityEditTV);
+        mSignInButton = (Button) findViewById(R.id.mainActivitySubmitButton);
+
+        mSignInButton.setOnClickListener(this);
 
         //GoogleApiClient
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -61,6 +80,20 @@ public class MainActivity extends AppCompatActivity
             nameTextView.setText(mFirebaseUser.getDisplayName());
             Glide.with(this).load(mFirebaseUser.getPhotoUrl()).override(320, 200).into(photoImageView);
         }
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i("SnapCat", dataSnapshot.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
@@ -89,5 +122,17 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.mainActivitySubmitButton:
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_POSTS);
+                ref.setValue(editText.getText().toString());
+                Log.i("SnapCat", "New Post:" + ref.getKey());
+                editText.setText("");
+                break;
+        }
     }
 }
